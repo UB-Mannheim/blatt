1573325ac101d0221f848cca71273d10f50710ec
shigapov <57352291+shigapov@users.noreply.github.com>
2022-09-13 08:42:56 +0200

    update to v.0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="blatt",
-    version='0.1.5',
+    version='0.1.6',
     author="Renat Shigapov",
     license="MIT",
     description="NLP-helper for OCR-ed pages in PAGE XML format.",
